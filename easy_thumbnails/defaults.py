a0ab9ad6f1638826f99be6a3ef0c6476793d93f1
Chris Beaven <smileychris@gmail.com>
2010-07-22 08:34:58 +1200

    Add a generate_source_image function to the engine, which will allow different methods for generating the image from the source file.

--- easy_thumbnails/defaults.py
+++ easy_thumbnails/defaults.py
@@ -17,3 +17,6 @@ PROCESSORS = (
     'easy_thumbnails.processors.scale_and_crop',
     'easy_thumbnails.processors.filters',
 )
+SOURCE_GENERATORS = (
+    'easy_thumbnails.source_generators.pil_image',
+)
