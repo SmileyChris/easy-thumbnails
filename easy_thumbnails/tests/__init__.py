d01a63aa9d9da02ce3991d09a1f38dc8f3e5451f
Graham King <graham@gkgk.org>
2011-06-24 18:16:58 -0700

    If FileManager.get_file can't find the thumb, optionally ask the storage and rebuild db cache if found. New setting to switch on that check. Tests.

--- easy_thumbnails/tests/__init__.py
+++ easy_thumbnails/tests/__init__.py
@@ -2,3 +2,4 @@ from easy_thumbnails.tests.fields import ThumbnailerFieldTest
 from easy_thumbnails.tests.files import FilesTest
 from easy_thumbnails.tests.processors import ScaleAndCropTest
 from easy_thumbnails.tests.templatetags import ThumbnailTagTest
+from easy_thumbnails.tests.models import FileManagerTest
