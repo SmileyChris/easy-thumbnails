7734502256fe7a9fc8f157fdd4e1baea442c45e3
vicalloy <zbirder@gmail.com>
2012-02-10 22:28:52 +0800

    * add  'def __init__(self, size="80x80", attrs=None)' for ImageClearableFileInput

--- easy_thumbnails/widgets.py
+++ easy_thumbnails/widgets.py
@@ -1,15 +1,23 @@
 from django.forms.widgets import ClearableFileInput
 from django.utils.safestring import mark_safe
-from django.utils.html import escape
+#from django.utils.html import escape
+from django.template import Context, Template
 
 class ImageClearableFileInput(ClearableFileInput):
     template_with_initial = u'%(clear_template)s<br />%(input_text)s: %(input)s'
+
+    def __init__(self, size="80x80", attrs=None):
+        self.size = size
+        super(ImageClearableFileInput, self).__init__(attrs)
 
     def img_id(self, name):
         return name + "_img_id"
 
     def render(self, name, value, attrs=None):
         html = super(ImageClearableFileInput, self).render(name, value, attrs)
-        img = u'<img id="%s" src="%s"/>' % (self.img_id(name), escape(value.url))
+        t = Template("{% load thumbnail %}{% thumbnail v " + self.size + " %}")
+        c = Context({"v": value})
+        img = u'<img id="%s" src="%s"/>' % (self.img_id(name), t.render(c))
+        img = u'<a href="%s" target="_blank">%s</a>' % (value.url, img)
         html = img + html
         return mark_safe(html)
