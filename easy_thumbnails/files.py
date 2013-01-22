c17d2c5731ae7f00dc72b69f893deae0b157e248
Chris Beaven <smileychris@gmail.com>
2013-01-23 11:02:25 +1300

    Small style change

--- easy_thumbnails/files.py
+++ easy_thumbnails/files.py
@@ -222,8 +222,8 @@ class ThumbnailFile(ImageFieldFile):
 
     def open(self, mode=None, *args, **kwargs):
         if self.closed and self.name:
-            self.file = self.storage.open(self.name,
-                mode or getattr(self, 'mode', None) or 'rb')
+            mode = mode or getattr(self, 'mode', None) or 'rb'
+            self.file = self.storage.open(self.name, mode)
         else:
             return super(ThumbnailFile, self).open(mode, *args, **kwargs)
 
@@ -476,8 +476,8 @@ class Thumbnailer(File):
 
     def open(self, mode=None):
         if self.closed:
-            self.file = self.source_storage.open(self.name,
-                mode or getattr(self, 'mode', None) or 'rb')
+            mode = mode or getattr(self, 'mode', None) or 'rb'
+            self.file = self.source_storage.open(self.name, mode)
         else:
             self.seek(0)
 
