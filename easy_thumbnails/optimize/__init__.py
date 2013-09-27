ac665517f8d861fa009f83f09b615206b6dbd2a8
Jacob Rief <jacob.rief@gmail.com>
2013-09-27 17:26:24 +0200

    Added signal handler which optionally optimizes thumbnails using external programs

---
+++ easy_thumbnails/optimize/__init__.py
@@ -0,0 +1,11 @@
+from django.dispatch import receiver
+from post_processor import optimize_thumbnail
+
+try:
+    from easy_thumbnails.signals import thumbnail_created
+
+    @receiver(thumbnail_created)
+    def thumbnail_created_callback(sender, **kwargs):
+        optimize_thumbnail(sender)
+except ImportError:
+    pass
