4f081dbdb724b546c35fcca1882ee1ba8cd81440
Chris Beaven <smileychris@gmail.com>
2014-08-13 14:52:06 +1200

    Python 3.4 testing

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
