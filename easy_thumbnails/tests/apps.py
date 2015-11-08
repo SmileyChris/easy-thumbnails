1c0ea1a102ed91342ce0d609733426b8a07cd67d
Chris Beaven <smileychris@gmail.com>
2015-11-09 09:54:15 +1300

    Fix an import error for old django versions
    Fixes #371

--- easy_thumbnails/tests/apps.py
+++ easy_thumbnails/tests/apps.py
@@ -1,4 +1,9 @@
-from django.apps import AppConfig
+try:
+    from django.apps import AppConfig
+except ImportError:
+    # Early Django versions import everything in test, avoid the failure due to
+    # AppConfig only existing in 1.7+
+    AppConfig = object
 
 
 class EasyThumbnailsTestConfig(AppConfig):
