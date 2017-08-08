b2aa938efb62908f7ac161dea36944d1b9dc6545
Chris Beaven <smileychris@gmail.com>
2017-08-08 22:59:09 +1000

    Fix a minor test failure on Python 3.6
    Update tox & travis to cover Python 3.5 and 3.6
    Now only testing Django 1.8 and 1.10+

--- easy_thumbnails/tests/settings_lts.py
+++
@@ -1,6 +0,0 @@
-"""
-Allow LTS version of Django to use the same test discovery runner method.
-"""
-from .settings_old import *   # NOQA
-
-TEST_RUNNER = 'discover_runner.DiscoverRunner'
--- easy_thumbnails/tests/settings_old.py
+++
@@ -1,8 +0,0 @@
-"""
-Don't add the test app to pre-1.7 versions of Django (since it's unnecessary
-and we use the new application registry).
-"""
-from .settings import *   # NOQA
-
-INSTALLED_APPS = INSTALLED_APPS[:]
-INSTALLED_APPS.remove('easy_thumbnails.tests.apps.EasyThumbnailsTestConfig')
