c4483dc44cb748749df420d9cd1f57fb4fac469b
Chris Beaven <smileychris@gmail.com>
2018-02-10 20:11:39 +1300

    Use proper environment markers for setup.py install_requires
    This should fix readthedocs not building too.

--- setup.py
+++ setup.py
@@ -50,8 +50,10 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'django>=1.8',
-        'pillow<4' if sys.version_info < (2, 7) else 'pillow',
+        'django>=1.8,<2;python_version<"3"',
+        'django>=1.8;python_version>="3"',
+        'pillow<4;python_version<"2.7"',
+        'pillow;python_version>="2.7"',
     ],
     cmdclass={'test': DjangoTests},
     classifiers=[
