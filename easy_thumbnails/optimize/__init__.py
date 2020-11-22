4bfebaf0af3442256ba58847d3e4ce74b3fb4def
Jacob Rief - Django-Migration <jacob.rief@uibk.ac.at>
2020-11-22 11:43:18 +0100

    readd some deprecation warnings

---
+++ easy_thumbnails/optimize/__init__.py
@@ -0,0 +1,4 @@
+from warnings import warn
+
+warn("The post-processor 'easy_thumbnails.optimize' has been removed from this project.\n"
+"Please remove it from your 'INSTALLED_APPS'.", DeprecationWarning)
